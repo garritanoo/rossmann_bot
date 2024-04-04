72b1b4dd10bd21ec634eab892995eb3540a2cc75
Juarez Garritano <garritanoo@gmail.com>
2024-04-04 07:21:29 -0300

    Update rossmann_bot.py

--- rossmann_bot.py
+++ rossmann_bot.py
@@ -109,6 +109,6 @@ def index():
     else:
         return '<h1>Rossmann Telegram Bot</h1>'
 
-if __name__ == 'main':
+if __name__ == '__main__':
     port = os.environ.get('PORT', 5000)
     app.run(host='0.0.0.0', port=port)
